4dda2b7aad03864b0805008f81a4af32253490be
Jeppe Moldrup <jeppemldrup@gmail.com>
2024-08-05 17:42:43 +0200

    feat: add hyprlock conf and add this to populate scipt

--- populate.sh
+++ populate.sh
@@ -6,3 +6,10 @@ then
 	echo "Creating symlink for sway config"
 	ln ./sway.conf ~/.config/sway/config
 fi
+
+mkdir -p ~/.config/hypr
+if [ ! -e ~/.config/hypr/hyprlock.conf ]
+then
+	echo "Creating symlink for hyprlock config"
+	ln ./hyprlock.conf ~/.config/hypr/hyprlock.conf
+fi
